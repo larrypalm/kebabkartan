69a17246ac8c14c5463b4aaa6090b842bef262b1
Gustav Palm <palm.gustav@gmail.com>
2025-06-04 23:57:15 +0200

    feat: ignore infrastrucure files in amplify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    typescript: {
+        // Exclude infrastructure and lambda files from Next.js type checking
+        ignoreBuildErrors: false,
+        tsconfigPath: 'tsconfig.json'
+    },
+    webpack: (config, { isServer }) => {
+        // Exclude infrastructure, lambda, and Amplify files from webpack build
+        config.watchOptions = {
+            ...config.watchOptions,
+            ignored: ['**/infrastructure/**', '**/lambda/**', '**/cdk.out/**', '**/amplify/**']
+        };
+        return config;
+    }
 }
 
 module.exports = nextConfig
