fb1636f118b7e3749fd9c773868773057cd5abd7
Gustav Palm <palm.gustav@gmail.com>
2025-06-10 20:02:53 +0200

    feat: add logs for test

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,14 @@ import "@aws-amplify/ui-react/styles.css";
 import dynamic from "next/dynamic";
 
 export default function App() {
+    const NEXT_PUBLIC_ADMIN_PASSWORD=process.env.NEXT_PUBLIC_ADMIN_PASSWORD
+    const NEXT_PUBLIC_API_URL=process.env.NEXT_PUBLIC_API_URL
+    const NEXT_PUBLIC_LAMBDA_PASSWORD=process.env.NEXT_PUBLIC_LAMBDA_PASSWORD
+
+    console.log('NEXT_PUBLIC_ADMIN_PASSWORD: ', NEXT_PUBLIC_ADMIN_PASSWORD)
+    console.log('NEXT_PUBLIC_API_URL: ', NEXT_PUBLIC_API_URL)
+    console.log('NEXT_PUBLIC_LAMBDA_PASSWORD: ', NEXT_PUBLIC_LAMBDA_PASSWORD)
+
     const Map = useMemo(() => dynamic(
         () => import('./components/Map'),
         { 
